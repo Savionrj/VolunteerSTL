905614dccdad832760ed8628a243b2396cc68a7e
Savionrj <savionrj@gmail.com>
2025-07-15 23:20:56 -0500

    initialized SpringBoot project, created database, application.properties

---
+++ volunteer-stl/src/main/java/com/unit_2_project/volunteer_stl/VolunteerStlApplication.java
@@ -0,0 +1,13 @@
+package com.unit_2_project.volunteer_stl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerStlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerStlApplication.class, args);
+	}
+
+}
---
+++ volunteer-stl/src/test/java/com/unit_2_project/volunteer_stl/VolunteerStlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit_2_project.volunteer_stl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerStlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
