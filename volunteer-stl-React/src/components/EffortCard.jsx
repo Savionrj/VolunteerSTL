d4c5730e006b9324d858718a26d5759c21ca0d12
Savionrj <savionrj@gmail.com>
2025-07-26 16:53:00 -0500

    Created base HTML structure for effort card

--- volunteer-stl-React/src/components/EffortCard.jsx
+++ volunteer-stl-React/src/components/EffortCard.jsx
@@ -1,6 +1,12 @@
+import volunteerImage from '../images/volunteer.jpg';
+
 export default function EffortCard({ effort }) {
   return (
-      <div className="flex flex-col border border-gray-500 rounded-md mt-10">
+      <div className="flex flex-col border border-gray-500 rounded-md mt-10 mx-20">
+        <img src={volunteerImage} alt="A volunteer with back turned to the camera" className="w-full h-48 object-cover rounded-t-md" />
+        <h3 className='text-2xl'>Title</h3>
+        <p>Date | Start Time</p>
+        <p>Organized By</p>
       </div>
   );
 }
