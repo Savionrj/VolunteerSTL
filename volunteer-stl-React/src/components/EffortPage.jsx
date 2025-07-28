1e2e3c1a7e281a6a02655fa46a664c677b511a01
Savionrj <savionrj@gmail.com>
2025-07-28 14:21:26 -0500

    Updated date parsing functionality, connected more effort detail fields to back-end data

--- volunteer-stl-React/src/components/EffortPage.jsx
+++ volunteer-stl-React/src/components/EffortPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import { useState } from "react";
+import volunteerImage from '../images/volunteer.jpg';
 
 export default function EffortPage({ efforts }) {
 
@@ -14,10 +15,30 @@ export default function EffortPage({ efforts }) {
     return <div>Effort not found.</div>;
   }
 
+
+  const formatDate = (effortDateTime) => {
+    const formattedDate = new Date(effortDateTime).toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric'
+    })
+
+    return formattedDate
+  }
+
+  const formatTime = (effortDateTime) => {
+    const formattedTime = new Date(effortDateTime).toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: '2-digit'
+    })
+
+    return formattedTime;
+  }
+
   const handleRegister = (e) => {
     setRegister(!register);
     //Will return after I create user registration functionality
-    }
+  }
 
   return (
     <div>
@@ -30,6 +51,31 @@ export default function EffortPage({ efforts }) {
         <button type="button" className="border border-gray-300 rounded-md p-2 text-2xl" onClick={(e) => handleRegister(e)}>
           {register ? "Unregister" : "Register"}
         </button>
+      </div>
+
+      <div>
+        <div className="flex justify-between items-center p-8">
+          <img src={volunteerImage} alt="A volunteer with back turned to the camera" className="w-1/2 h-full object-cover rounded-md" />
+          <div className="flex flex-col w-full text-2xl px-10">
+            <h4 className="text-3xl">Details</h4>
+            <p>Date: {formatDate(currentEffort.startTime)}</p>
+            <p>Starting Time: {formatTime(currentEffort.startTime)} </p>
+            <p>Ending Time: {formatTime(currentEffort.endTime)} </p>
+            <p>Location: {currentEffort.address}
+              <br />
+              {currentEffort.city}, {currentEffort.state} {currentEffort.zipCode}
+            </p>
+            <p>Tags: {currentEffort.tags.map(tag => tag.name).join(', ')}
+            </p>
+            <p>Volunteer Count: {currentEffort.maxVolunteerCount} </p>
+          </div>
+        </div>
+
+        <div>
+        </div>
+
+        <div>
+        </div>
       </div>
     </div>
   )
