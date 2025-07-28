8c6131853b14e73d94559adadbf61613d0651242
Savionrj <savionrj@gmail.com>
2025-07-28 12:39:56 -0500

    Created state for register button, created handleRegister function

--- volunteer-stl-React/src/components/EffortPage.jsx
+++ volunteer-stl-React/src/components/EffortPage.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom"
+import { useState } from "react";
 
 export default function EffortPage({ efforts }) {
 
-  const {effortId} = useParams();
+  const { effortId } = useParams();
+  const [register, setRegister] = useState(false)
 
   const currentEffort = efforts.find(
     (effort) => effort.effortId.toString() === effortId
@@ -12,16 +14,21 @@ export default function EffortPage({ efforts }) {
     return <div>Effort not found.</div>;
   }
 
+  const handleRegister = (e) => {
+    setRegister(!register);
+    //Will return after I create user registration functionality
+    }
+
   return (
     <div>
       <div className="flex justify-between items-center px-10 py-6 border-b border-gray-300">
         <h3>
           <span className="text-3xl">{currentEffort.effortName}</span>
-        <br/>
-        <span className="text-2xl">By {currentEffort.organizerName}</span>
+          <br />
+          <span className="text-2xl">By {currentEffort.organizerName}</span>
         </h3>
-        <button>
-          Register
+        <button type="button" className="border border-gray-300 rounded-md p-2 text-2xl" onClick={(e) => handleRegister(e)}>
+          {register ? "Unregister" : "Register"}
         </button>
       </div>
     </div>
